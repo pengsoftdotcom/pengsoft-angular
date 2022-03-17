a76b7cefcd2aec81397fd13e4776904b2f8bd421
peng.dang <peng.dang@pengsoft.com>
2022-03-17 20:08:24 +0800

    init

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pengsoft-app-admin'),
+      dir: require('path').join(__dirname, './coverage/pengsoft-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
